2c12c342b738833d374e497a05f9c658ad5b3bb5
hyolics <whi20021201@gmail.com>
2025-02-20 11:35:49 +0800

    Update overview_index.sql

--- dags/scripts/overview_index.sql
+++ dags/scripts/overview_index.sql
@@ -6,7 +6,7 @@ select
 	COUNT(DISTINCT invoice_no) AS unique_transactions,
 	COUNT(DISTINCT CASE WHEN invoice_no NOT LIKE 'C%' THEN invoice_no END) AS success_transactions,
     COUNT(DISTINCT CASE WHEN invoice_no LIKE 'C%' THEN invoice_no END) AS cancel_transactions,
-    ROUND(SUM(unit_price * quantity)::numeric, 2) AS amount,
+    ROUND(SUM(unit_price * quantity)::numeric, 2) AS revenue,
     ROUND(SUM(unit_price * quantity)/count(DISTINCT CASE WHEN customer_id != 'NaN' THEN customer_id END)) as avg_by_customer,
     ROUND(SUM(unit_price * quantity)/COUNT(DISTINCT CASE WHEN invoice_no NOT LIKE 'C%' THEN invoice_no END)) as avg_by_transaction
 FROM invoices;
