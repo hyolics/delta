5426910ce28b634831ee92b4612ad1bdec61f38a
hyolics <whi20021201@gmail.com>
2025-02-20 11:36:02 +0800

    Update overview_by_months.sql

--- dags/scripts/overview_by_months.sql
+++ dags/scripts/overview_by_months.sql
@@ -5,7 +5,7 @@ SELECT
     CAST(DATE_TRUNC('MONTH', invoice_date) AS DATE) AS mon,
     COUNT(DISTINCT CASE WHEN customer_id != 'NaN' THEN customer_id END) AS unique_customers,
     COUNT(DISTINCT invoice_no) AS unique_transactions,
-    ROUND(SUM(unit_price * quantity)::numeric, 2) AS amount
+    ROUND(SUM(unit_price * quantity)::numeric, 2) AS revenue
 FROM invoices
 WHERE invoice_date > '2010-12-31'
 GROUP BY DATE_TRUNC('MONTH', invoice_date);
